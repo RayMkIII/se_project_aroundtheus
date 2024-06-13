d17d2c303740c43909fa4586769d037e1d91aca2
Daniel Galvez <galvez.juan.d@gmail.com>
2024-06-12 22:31:32 -0400

    add profile form functionality

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,10 +1,27 @@
-let openButton = document.querySelector(".profile__edit-button");
-let modal = document.querySelector(".modal");
-let closeButton = document.querySelector(".modal__close-button");
+const openButton = document.querySelector(".profile__edit-button");
+const modal = document.querySelector(".modal");
+const closeButton = document.querySelector(".modal__close-button");
+const submitButton = document.querySelector(".form__save-button");
+
+const profileName = document.querySelector(".profile__name");
+const profileDesc = document.querySelector(".profile__description");
+const inputName = document.querySelector(".form__input-name");
+const inputDesc = document.querySelector(".form__input-description");
 
 openButton.addEventListener("click", function () {
+  inputName.value = profileName.textContent;
+  inputDesc.value = profileDesc.textContent;
   modal.classList.add("modal__opened");
 });
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileDesc.textContent = inputDesc.value;
+  modal.classList.remove("modal__opened");
+}
+
+submitButton.addEventListener("click", handleProfileFormSubmit);
 
 closeButton.addEventListener("click", function () {
   modal.classList.remove("modal__opened");
