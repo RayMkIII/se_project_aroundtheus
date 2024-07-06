5aabc43e5c47e690f92a2e032e5886d158063e47
Daniel Galvez <galvez.juan.d@gmail.com>
2024-07-06 17:22:49 -0400

    refactor: combine modal.js into index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+//initial posts
 const cardGallery = document.querySelector(".posts");
 
 const object1 = {
@@ -32,16 +33,38 @@ const object6 = {
 
 const initialCards = [object1, object2, object3, object4, object5, object6];
 
-function getCardElement(data) {
+//profile modal
+const profileOpenButton = document.querySelector(".profile__edit-button");
+const profileModal = document.querySelector(".profile-modal");
+const profileForm = profileModal.querySelector(".form");
+
+const profileName = document.querySelector(".profile__name");
+const profileDesc = document.querySelector(".profile__description");
+const inputName = document.querySelector(".form__input-name");
+const inputDesc = document.querySelector(".form__input-description");
+
+//post modal
+const postOpenButton = document.querySelector(".profile__post-button");
+const postModal = document.querySelector(".post-modal");
+const postForm = postModal.querySelector(".form");
+
+const inputTitle = postModal.querySelector(".form__input-title");
+const inputLink = postModal.querySelector(".form__input-link");
+
+//image modal
+const imageModal = document.querySelector(".image-modal");
+
+//close buttons - all modals
+const closeButtons = document.querySelectorAll(".modal__close-button");
+
+function getCardElement(title, link) {
   const cardTemplate = document.querySelector("#post").content;
   const cardElement = cardTemplate.querySelector(".post").cloneNode(true);
-  cardElement.querySelector(".post__image").src = data.link;
-  cardElement.querySelector(".post__image").alt = data.name;
-  cardElement.querySelector(".post__title").textContent = data.name;
+  cardElement.querySelector(".post__image").src = link;
+  cardElement.querySelector(".post__image").alt = title;
+  cardElement.querySelector(".post__title").textContent = title;
 
-  const postImage = cardElement.querySelector(".post__image");
-  postImage.addEventListener("click", () => {
-    const imageModal = document.querySelector(".image-modal");
+  cardElement.querySelector(".post__image").addEventListener("click", () => {
     imageModal.querySelector(".modal__image").src =
       cardElement.querySelector(".post__image").src;
     imageModal.querySelector(".modal__image").alt =
@@ -66,6 +89,43 @@ function getCardElement(data) {
 }
 
 initialCards.forEach((item) => {
-  const cardElement = getCardElement(item);
+  const cardElement = getCardElement(item.name, item.link);
   cardGallery.prepend(cardElement);
+});
+
+const toggleModal = (modal) => {
+  modal.classList.toggle("modal_opened");
+};
+
+profileOpenButton.addEventListener("click", function () {
+  inputName.value = profileName.textContent;
+  inputDesc.value = profileDesc.textContent;
+  toggleModal(profileModal);
+});
+
+postOpenButton.addEventListener("click", () => {
+  toggleModal(postModal);
+});
+
+profileForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileDesc.textContent = inputDesc.value;
+  toggleModal(profileModal);
+});
+
+postForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const newPost = getCardElement(inputTitle.value, inputLink.value);
+  cardGallery.prepend(newPost);
+  inputTitle.value = "Title";
+  inputLink.value = "Image URL";
+  toggleModal(postModal);
+});
+
+closeButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const parentModal = button.closest(".modal");
+    toggleModal(parentModal);
+  });
 });
